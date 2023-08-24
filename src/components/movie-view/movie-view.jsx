55d725542db73482916ccd5363eb74f7a951c62a
thisjunghyewon <this.junghyewon@gmail.com>
2023-08-24 13:22:41 +0200

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,48 +1,52 @@
-import PropTypes from "prop-types";
-
-//information for movie information displayed once user clicks a movie
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.ImagePath} />
+        <img src={movie.Image} style={{ width: "500px" }} />
       </div>
       <div>
         <span>Title: </span>
         <span>{movie.Title}</span>
       </div>
       <div>
-        <span>Genre: </span>
-        <span>{movie.Genre.Name}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.Director.Name} </span>
+        <span>Release: </span>
+        <span>{movie.Release}</span>
       </div>
       <div>
         <span>Description: </span>
         <span>{movie.Description}</span>
       </div>
       <div>
+        <span>Director: </span>
+        <span>{movie.Director.Name} </span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name} </span>
+      </div>
+      <div>
         <span>Featured: </span>
-        <span>{movie.Featured}</span>
+        <span>{movie.Featured} </span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
 };
 
-//PropType conditions for moviesFromAPI variables return statement in main-view.jsx
 MovieView.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     Title: PropTypes.string.isRequired,
+    Release: PropTypes.string.isRequired,
+    Image: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Cast: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
     }),
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
     }),
-    Description: PropTypes.string.isRequired,
     Featured: PropTypes.string.isRequired,
   }).isRequired,
   onBackClick: PropTypes.func.isRequired,
