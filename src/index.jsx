8901c81db6d114fe7c612873ff5789079a6cc607
thisjunghyewon <this.junghyewon@gmail.com>
2023-09-04 15:59:41 +0200

    Install React bootstrap
    Install React Bootstrap by running npm install react-bootstrap bootstrap, then import bootstrap in src/index.scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view.jsx";
 
 // Import statement to indicate that you need to bundle `./index.scss`
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 // Main component (will eventually use all the others)
