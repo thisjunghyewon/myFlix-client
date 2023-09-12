fe275dcf66f997e4738934abe9eb1bfd79322cbf
thisjunghyewon <this.junghyewon@gmail.com>
2023-09-12 17:46:49 +0200

    correct

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Button, Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
 // import { useSelector } from "react-redux";
